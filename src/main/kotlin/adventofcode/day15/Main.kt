a05be335e0373ec69b29d9517339615e9d822a5d
brsantos <brunorene@gmail.com>
2019-12-19 03:20:18 +0000

    advent of code 2019 #15 BFS

--- src/main/kotlin/adventofcode/day15/Main.kt
+++ src/main/kotlin/adventofcode/day15/Main.kt
@@ -5,7 +5,7 @@ import adventofcode.day5.readProgram
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.runBlocking
 import java.io.File
-import kotlin.random.Random
+import java.util.*
 import kotlin.system.exitProcess
 
 fun main() {
@@ -13,7 +13,48 @@ fun main() {
     day15star2()
 }
 
-data class Point(val x: Long = 0, val y: Long = 0)
+class Point(val x: Long = 0, val y: Long = 0, private val depth: Int = 0) {
+    fun nextPosition(dir: Direction) = when (dir) {
+        North -> Point(x, y - 1, depth + 1)
+        South -> Point(x, y + 1, depth + 1)
+        West -> Point(x - 1, y, depth + 1)
+        East -> Point(x + 1, y, depth + 1)
+    }
+
+    fun nextDirection(p: Point) = when {
+        x == p.x && y < p.y -> North
+        x == p.x && y > p.y -> South
+        x < p.x && y == p.y -> East
+        x > p.x && y == p.y -> West
+        else -> throw RuntimeException("it is the same node!")
+    }
+
+    fun neighbours() = listOf(North, South, East, West).map { nextPosition(it) }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Point
+
+        if (x != other.x) return false
+        if (y != other.y) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = x.hashCode()
+        result = 31 * result + y.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "Point(x=$x, y=$y, depth=$depth)"
+    }
+
+
+}
 
 sealed class Tile(val char: Char)
 object Wall : Tile('#')
@@ -22,8 +63,8 @@ object Oxygen : Tile('X')
 object Unknown : Tile(' ')
 
 fun tile(id: Long) = when (id) {
-    0L -> Empty
-    1L -> Wall
+    0L -> Wall
+    1L -> Empty
     2L -> Oxygen
     else -> throw RuntimeException("unknown tile")
 }
@@ -34,61 +75,32 @@ object South : Direction(2)
 object West : Direction(3)
 object East : Direction(4)
 
-fun direction(id: Long) = when (id) {
-    1L -> North
-    2L -> South
-    3L -> West
-    4L -> East
-    else -> throw RuntimeException("unknown direction")
-}
-
-fun Point.nextPosition(dir: Direction) = when (dir) {
-    North -> Point(x, y - 1)
-    South -> Point(x, y + 1)
-    West -> Point(x - 1, y)
-    East -> Point(x + 1, y)
-}
-
 object Hull {
     private val data = mutableMapOf<Point, Tile>()
 
-    private fun tilePlace(p: Point) = data[p] ?: Unknown
-
-    private fun allVisited(now: Point): Set<Direction> {
-        val directions = mutableSetOf<Direction>()
-        if (data.containsKey(now.nextPosition(North)))
-            directions.add(North)
-        if (data.containsKey(now.nextPosition(South)))
-            directions.add(South)
-        if (data.containsKey(now.nextPosition(West)))
-            directions.add(West)
-        if (data.containsKey(now.nextPosition(East)))
-            directions.add(East)
-        return directions
-    }
+    fun tilePlace(p: Point) = data[p] ?: Unknown
 
     fun set(p: Point, t: Tile) {
         data[p] = t
     }
 
     fun draw() {
-        ((data.keys.map { it.y }.min() ?: 0) until (data.keys.map { it.y }.max() ?: 0)).forEach { y ->
-            ((data.keys.map { it.x }.min() ?: 0) until (data.keys.map { it.x }.max() ?: 0)).forEach { x ->
-                print(tilePlace(Point(x, y)).char)
+        File("hull.draw").bufferedWriter().use { writer ->
+            ((data.keys.map { it.y }.min() ?: 0)..(data.keys.map { it.y }.max() ?: 0)).forEach { y ->
+                ((data.keys.map { it.x }.min() ?: 0)..(data.keys.map { it.x }.max() ?: 0)).forEach { x ->
+                    writer.write(tilePlace(Point(x, y)).char.toString())
+                }
+                writer.newLine()
             }
-            println()
         }
     }
 
     fun randomDirection(now: Point): Direction {
-        var direction = direction(Random.nextLong(4) + 1L)
-        if (allVisited(now).size < 4)
-            while (allVisited(now).contains(direction)) {
-                direction = direction(Random.nextLong(4) + 1L)
-            }
-        else
-            direction = direction(Random.nextLong(4) + 1L)
-        return direction
+        val possibleDirections = listOf(North, South, East, West)
+                .filter { tilePlace(now.nextPosition(it)) != Wall }
+        val unknownDirection = possibleDirections
+                .filter { tilePlace(now.nextPosition(it)) == Unknown }
+        return if (unknownDirection.isNotEmpty()) unknownDirection.random() else possibleDirections.random()
     }
 }
 
@@ -96,29 +108,38 @@ fun day15star1() {
     val memory = readProgram(File("day15.txt"))
     val channel = Channel<Long>(1000)
     var currentPosition = Point()
+    var toVisit = LinkedList<Point>()
+    Hull.set(currentPosition, Empty)
+    currentPosition.neighbours().forEach { toVisit.push(it) }
     runBlocking {
-        var direction = Hull.randomDirection(currentPosition)
+        var visiting = toVisit.pop()
+        var direction = currentPosition.nextDirection(visiting)
         channel.send(direction.id)
-        computer(memory, channel) {
-            when (tile(it)) {
-                Wall -> {
-                    val wallPos = currentPosition.nextPosition(direction)
-                    Hull.set(wallPos, Wall)
-                    direction = Hull.randomDirection(currentPosition)
-                }
+        computer(memory, channel) { tile ->
+            when (tile(tile)) {
+                Wall -> Hull.set(visiting, Wall)
                 Empty -> {
-                    currentPosition = currentPosition.nextPosition(direction)
-                    Hull.set(currentPosition, Empty)
-                    direction = Hull.randomDirection(currentPosition)
+                    currentPosition = visiting
+                    Hull.set(visiting, Empty)
+                    currentPosition.neighbours()
+                            .filter { Hull.tilePlace(it) == Unknown }
+                            .forEach { toVisit.push(it) }
                 }
                 Oxygen -> {
-                    currentPosition = currentPosition.nextPosition(direction)
-                    Hull.set(currentPosition, Oxygen)
-                    println("Found Oxygen Station on $currentPosition")
+                    Hull.set(visiting, Oxygen)
+                    println("Found Oxygen Station on $visiting")
                     Hull.draw()
                     exitProcess(0)
                 }
             }
+            do {
+                visiting = toVisit.pop()
+                if (visiting.x != currentPosition.x && visiting.y != currentPosition.y)
+                    println("impossible direction -> $visiting / $currentPosition")
+            } while (visiting.x != currentPosition.x && visiting.y != currentPosition.y)
+            println("Visiting $currentPosition -> $visiting")
+            direction = currentPosition.nextDirection(visiting)
+            println("Visiting $visiting -> $direction")
             channel.send(direction.id)
         }
     }
