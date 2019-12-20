539018ceca4465449c21d180b682c1b96fd045e1
brsantos <brunorene@gmail.com>
2019-12-20 23:25:40 +0000

    advent of code 2019 #15 part 1

--- src/main/kotlin/adventofcode/day15/Main.kt
+++ src/main/kotlin/adventofcode/day15/Main.kt
@@ -2,11 +2,12 @@ package adventofcode.day15
 
 import adventofcode.day5.computer
 import adventofcode.day5.readProgram
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.io.File
-import java.util.*
-import kotlin.system.exitProcess
 
 fun main() {
     day15star1()
@@ -14,22 +15,15 @@ fun main() {
 }
 
 class Point(val x: Long = 0, val y: Long = 0, private val depth: Int = 0) {
-    fun nextPosition(dir: Direction) = when (dir) {
+
+    private fun nextPosition(dir: Direction) = when (dir) {
         North -> Point(x, y - 1, depth + 1)
         South -> Point(x, y + 1, depth + 1)
         West -> Point(x - 1, y, depth + 1)
         East -> Point(x + 1, y, depth + 1)
     }
 
-    fun nextDirection(p: Point) = when {
-        x == p.x && y < p.y -> North
-        x == p.x && y > p.y -> South
-        x < p.x && y == p.y -> East
-        x > p.x && y == p.y -> West
-        else -> throw RuntimeException("it is the same node!")
-    }
-
-    fun neighbours() = listOf(North, South, East, West).map { nextPosition(it) }
+    fun neighbours() = listOf(North, South, East, West).map { it to nextPosition(it) }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -94,55 +88,49 @@ object Hull {
             }
         }
     }
+}
 
-    fun randomDirection(now: Point): Direction {
-        val possibleDirections = listOf(North, South, East, West)
-                .filter { tilePlace(now.nextPosition(it)) != Wall }
-        val unknownDirection = possibleDirections
-                .filter { tilePlace(now.nextPosition(it)) == Unknown }
-        return if (unknownDirection.isNotEmpty()) unknownDirection.random() else possibleDirections.random()
+suspend fun depthFirst(memory: MutableMap<Long, Long>, channel: Channel<Long>, current: Point, next: Point) {
+    coroutineScope {
+        computer(memory, channel) { tileCode ->
+            when (tile(tileCode)) {
+                Wall -> {
+                    Hull.set(current, Wall); println("Wall $next!")
+                }
+                Empty -> {
+                    println("Empty $next!")
+                    Hull.set(next, Empty)
+                    next.neighbours()
+                            .filter { Hull.tilePlace(it.second) == Unknown }
+                            .map { neighbour ->
+                                val newChannel = Channel<Long>(10).apply { send(neighbour.first.id) }
+                                launch { depthFirst(memory.toMutableMap(), newChannel, next, neighbour.second) }
+                            }
+                }
+                Oxygen -> {
+                    Hull.set(next, Oxygen)
+                    println("Found Oxygen Station on $next")
+                }
+            }
+        }
     }
 }
 
+
 fun day15star1() {
     val memory = readProgram(File("day15.txt"))
-    val channel = Channel<Long>(1000)
-    var currentPosition = Point()
-    var toVisit = LinkedList<Point>()
-    Hull.set(currentPosition, Empty)
-    currentPosition.neighbours().forEach { toVisit.push(it) }
-    runBlocking {
-        var visiting = toVisit.pop()
-        var direction = currentPosition.nextDirection(visiting)
-        channel.send(direction.id)
-        computer(memory, channel) { tile ->
-            when (tile(tile)) {
-                Wall -> Hull.set(visiting, Wall)
-                Empty -> {
-                    currentPosition = visiting
-                    Hull.set(visiting, Empty)
-                    currentPosition.neighbours()
-                            .filter { Hull.tilePlace(it) == Unknown }
-                            .forEach { toVisit.push(it) }
-                }
-                Oxygen -> {
-                    Hull.set(visiting, Oxygen)
-                    println("Found Oxygen Station on $visiting")
-                    Hull.draw()
-                    exitProcess(0)
-                }
-            }
-            do {
-                visiting = toVisit.pop()
-                if (visiting.x != currentPosition.x && visiting.y != currentPosition.y)
-                    println("impossible direction -> $visiting / $currentPosition")
-            } while (visiting.x != currentPosition.x && visiting.y != currentPosition.y)
-            println("Visiting $currentPosition -> $visiting")
-            direction = currentPosition.nextDirection(visiting)
-            println("Visiting $visiting -> $direction")
-            channel.send(direction.id)
+    val start = Point()
+    Hull.set(start, Empty)
+    val job = GlobalScope.launch {
+        start.neighbours().map { neighbour ->
+            val newChannel = Channel<Long>(10).apply { send(neighbour.first.id) }
+            launch { depthFirst(memory.toMutableMap(), newChannel, start, neighbour.second) }
         }
     }
+    runBlocking {
+        job.join()
+    }
+    Hull.draw()
 }
 
 fun day15star2() {
