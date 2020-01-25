ca9ce09baae7b399b406471432d2ec16c3ac5259
brsantos <brunorene@gmail.com>
2020-01-25 10:09:44 +0000

    day 16 fast part 2!!

--- src/main/kotlin/adventofcode/day16/Main.kt
+++ src/main/kotlin/adventofcode/day16/Main.kt
@@ -2,7 +2,6 @@ package adventofcode.day16
 
 import java.io.File
 import kotlin.math.abs
-import kotlin.math.ceil
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
@@ -16,30 +15,35 @@ fun readFrequency() = File("day16.txt")
         .readText()
         .map { it.toString().toLong() }
 
-fun basePattern(index: Int, length: Int): List<Byte> {
-    val list0 = mutableListOf<Byte>()
-    val list1 = mutableListOf<Byte>()
-    val listMinus1 = mutableListOf<Byte>()
-    repeat(index + 1) {
-        list0.add(0)
-        list1.add(1)
-        listMinus1.add(-1)
-    }
-    val repeat = ceil(length.toDouble() / ((index + 1) * 4)).toLong() + 1L
-    return (1..repeat).flatMap { list0 + list1 + list0 + listMinus1 }
-}
-
 private fun fft(input: List<Long>): List<Long> {
-    var freq = input
-    val patterns = freq.indices.associateWith { basePattern(it, freq.size) }
-    (0 until 100).forEach {
-        println("phase $it")
-        freq = freq.indices.map { index ->
+    val freq = input.toMutableList()
+    for (repeat in 1..100) {
+        println("repeat $repeat")
+        for (index in 0 until freq.size) {
+            val negativeStart = 2 + 3 * index
+            val divider = 4 + 4 * index
             var sum = 0L
             for (fIdx in index until freq.size) {
-                sum += patterns[index]!![fIdx + 1] * freq[fIdx]
+                sum += when {
+                    ((fIdx - index) / (index + 1)) % 4 == 0 -> freq[fIdx]
+                    fIdx >= negativeStart && fIdx % divider in (negativeStart..negativeStart + index) -> -freq[fIdx]
+                    else -> 0
+                }
             }
-            abs(sum % 10)
+            freq[index] = abs(sum % 10)
+        }
+    }
+    return freq
+}
+
+private fun fft2(tail: List<Long>): List<Long> {
+    val freq = tail.toMutableList()
+    for (repeat in 1..100) {
+        var lastSum = 0L
+        println("repeat $repeat")
+        for (index in (freq.size - 1) downTo 0) {
+            lastSum += freq[index]
+            freq[index] = lastSum % 10
         }
     }
     return freq
@@ -56,7 +60,7 @@ fun day16star1() {
 
 fun day16star2() {
     val freq = readFrequency()
-    val result = fft((1..10000).flatMap { freq })
-    val offset = result.take(7).joinToString("").toInt()
-    println(result.drop(offset).take(8).joinToString(""))
+    val offset = freq.take(7).joinToString("").toInt()
+    val result = fft2((1..10000).flatMap { freq }.drop(offset))
+    println(result.take(8).joinToString(""))
 }
